11690648fe589635beeee0fd3bcd5c98a38e2397
anushka2998 <161566140+anushka2998@users.noreply.github.com>
2024-02-28 00:24:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const startButton = document.getElementById('startButton');
 
-startButton.addEventListener('click', () => {
-    // Add code to hide landing page and transition to your chatbot interface
-});
