b3e8d0ff6431d78f1b885ec2682667a58ea58ccc
anushka2998 <161566140+anushka2998@users.noreply.github.com>
2024-02-29 00:01:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    var videos = ["video1.mp4", "video2.mp4", "video3.mp4","video4.mp4"]; // Add your video file names here
+    var currentVideoIndex = 0;
+    var heroVideo = document.getElementById('heroVideo');
+    var prevButton = document.getElementById('prevButton');
+    var nextButton = document.getElementById('nextButton');
+
+    prevButton.addEventListener('click', function() {
+        if (currentVideoIndex > 0) {
+            currentVideoIndex--;
+            updateVideoSource();
+        }
+    });
+
+    nextButton.addEventListener('click', function() {
+        if (currentVideoIndex < videos.length - 1) {
+            currentVideoIndex++;
+            updateVideoSource();
+        }
+    });
+
+    function updateVideoSource() {
+        heroVideo.src = videos[currentVideoIndex];
+        heroVideo.load();
+        heroVideo.play();
+    }
+});
