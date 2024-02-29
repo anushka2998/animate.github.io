6eb5ad7c22d870639731e6b81e5c27a7e8f21f6e
anushka2998 <161566140+anushka2998@users.noreply.github.com>
2024-02-29 00:02:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    var videos = ["video1.mp4", "video2.mp4", "video3.mp4","video4.mp4"]; // Add your video file names here
+    var videos = ["Video1.mp4", "video2.mp4", "video3.mp4","video4.mp4"]; // Add your video file names here
     var currentVideoIndex = 0;
     var heroVideo = document.getElementById('heroVideo');
     var prevButton = document.getElementById('prevButton');
