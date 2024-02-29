b736f8c636ea367a73a7ab5073a0abcdced6109a
anushka2998 <161566140+anushka2998@users.noreply.github.com>
2024-02-29 00:10:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     var videos = ["Video1.mp4", "video2.mp4", "video3.mp4","video4.mp4"]; // Add your video file names here
     var currentVideoIndex = 0;
     var heroVideo = document.getElementById('heroVideo');
     var prevButton = document.getElementById('prevButton');
     var nextButton = document.getElementById('nextButton');
+
+    // Load the initial video
+    updateVideoSource();
 
     prevButton.addEventListener('click', function() {
         if (currentVideoIndex > 0) {
